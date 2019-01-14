a19b8c3c98465f1d20ebfe6795fa019089efcc87
kanishkamisra <menogetusername@gmail.com>
2019-01-13 19:13:23 -0500

    organization of error sets

--- R/Error analysis.R
+++ R/Error analysis.R
@@ -1,37 +1,36 @@
 library(tidyverse)
 
 errors <- read_csv("data/parsed.csv")
-answers <- read_csv("data/answers_parsed.csv")
 
-errors %>%
-  select(i, c, type) %>%
-  View()
-
-errors %>%
+content_errors <- errors %>%
   filter(!is.na(i), !is.na(c)) %>%
-  mutate(
-    lv_dist = stringdist::stringdist(i, c, method = "lv")
-  ) %>%
-  select(language, age, type, i, c, lv_dist) %>%
-  filter(lv_dist > 3) %>%
-  View()
-
-answers %>% View()
-
-errors %>%
-  filter(!is.na(i), !is.na(c)) %>%
-  filter(type %in% c("RJ", "RN", "RV", "RY", "RQ")) %>%
+  filter(type %in% c("RJ", "RN", "RV", "RY")) %>%
   select(language, type, i, c, file_name) %>%
   mutate(
+    id = paste("person", str_extract(file_name, "(?<=doc)(.*)(?=\\.xml)"), sep = "_"),
     i_count = map_int(i, ~length(str_split(.x, " ", simplify = T))),
     c_count = map_int(c, ~length(str_split(.x, " ", simplify = T))),
   ) %>%
+  select(-file_name)
+
+single_word_errors <- content_errors %>%
   filter(i_count == 1, c_count == 1) %>%
+  select(-i_count, -c_count)
+
+multi_word_errors <- content_errors %>%
+  filter(i_count > 1 | c_count > 1) %>%
+  select(-i_count, -c_count)
+
+content_errors %>%
   count(language, sort = T)
 
-errors %>%
-  filter(!is.na(i), !is.na(c)) %>%
-  filter(str_detect(type, "R")) %>%
-  filter(!(type %in% c("RP", "R"))) %>%
-  count(language, sort = T)
+single_word_errors %>%
+  count(id, language, sort = T)
 
+# unique people making content word mistakes (single word only) = 1199
+
+single_word_errors %>%
+  distinct() %>%
+  count(language, sort = TRUE)
+
+single_word_errors
