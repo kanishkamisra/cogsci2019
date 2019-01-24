0bb101bbbfec45f6eb945ef93644b5b9fef3fdd5
Kanishka Misra <menogetusername@gmail.com>
2019-01-24 11:28:33 -0500

    polyglot overlaps

--- R/polyglot_analysis.R
+++ R/polyglot_analysis.R
@@ -1,7 +1,23 @@
 library(tidyverse)
 
-polyglot_experiment <- read_csv("data/ms_final_polyglot_english.csv")
+polyglot_experiment <- read_csv("data/ms_final_with_polyglot_2.csv")
 
 polyglot_experiment %>%
-  select_at(vars("c", "i", "l1_c", "l1_i", "id", contains("polyglot"))) %>%
-  filter(l2_sim_polyglot_5 == -10)
+  select_at(vars("c", "i", "l1_c", "l1_i", "id", "language", contains("polyglot_10"))) %>%
+  add_count(language) %>%
+  group_by(language) %>%
+  mutate(
+    missing = mean(l1_sim_polyglot_10 == -10 | l2_sim_polyglot_10 == -10)
+  ) %>%
+  distinct(language, n, missing) %>%
+  arrange(-missing)
+
+polyglot_experiment %>%
+  select_at(vars("c", "i", "l1_c", "l1_i", "id", "language", contains("polyglot_10"))) %>%
+  filter(language == "German") %>%
+  filter(l1_sim_polyglot_10 == -10)
+
+polyglot_experiment %>%
+  filter(l1_sim_polyglot_10 != -10 | l2_sim_polyglot_10 != -10) %>%
+  filter(language != "Japanese") %>%
+  write_csv("data/polyglot_experiment_final.csv")
